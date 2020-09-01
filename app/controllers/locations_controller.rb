521baa9b2a07ba6fa257b6136644924d842bc039
Tomine Odegard <tomineodegard@hotmail.no>
2020-09-01 21:30:41 +0200

    Location model, create and new method and corresponding view - needs css

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,20 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.all
   end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    @location.save
+    redirect_to locations_path(@location)
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :adress, :description, :price)
+  end
 end
