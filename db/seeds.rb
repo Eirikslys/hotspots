05e42c1bc7d54de3668d54a0c1ba214f73ce0746
eirik lysenstoen <elysenstoen@gmail.com>
2020-09-05 16:17:08 +0200

    edited seed for the demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,10 +22,14 @@ puts "users destroyed"
 # Generate some users, they are identical but with a number from 1 to 5 before their email.
 puts "creating users"
 c = 1
-5.times do
+u = User.new(email: "mail@mail.com", password: "password")
+u.save!
+p u
+4.times do
   User.create!(email: "#{c}mail@mail.com", password: "password")
   c += 1
 end
+
 puts User.all
 
 # Generate locations, owndership by users is assigned randomly
@@ -34,12 +38,12 @@ file = File.open("app/assets/images/garden.jpg")
 Location.create!(name:"A secret garden", user: User.all.sample, address: "Braakmakergaten 27", price: "299$", description:"Enact a romantic rendesvouz or dramatic meeting in our wellkept walled garden, complete with statues")
 loc = Location.last
 loc.photo.attach(io: file, filename: 'garden.jpg', content_type: 'image/png')
-Location.create!(name:"Cocaine mansion with great view", user: User.all.sample, address: "High street 477", price: "349$", description:"A mini-palace almost tastefully executed with authentic Chinese marble. Live out your scarface fantasy here")
+Location.create!(name:"Coke mansion", user: User.all.sample, address: "High street 477", price: "349$", description:"A mini-palace almost tastefully executed with authentic Chinese marble. Live out your scarface fantasy here")
 file = File.open("app/assets/images/mansion.jpg")
 loc = Location.last
 loc.photo.attach(io: file, filename: 'mansion.jpg', content_type: 'image/png')
 
-Location.create!(name:"Cozy cabin", user: User.all.sample, address: "Timber Road 4228", price: "120$", description:"This solid lumber cabin, built in 1932, will give your pictures a homely and rustique feel. Inquire for snowscooter rental.")
+Location.create!(name:"Cozy cabin", user: u, address: "Timber Road 4228", price: "120$", description:"This solid lumber cabin, built in 1932, will give your pictures a homely and rustique feel. Inquire for snowscooter rental.")
 file = File.open("app/assets/images/cabin.jpg")
 loc = Location.last
 loc.photo.attach(io: file, filename: 'cabin.jpg', content_type: 'image/png')
@@ -52,6 +56,6 @@ loc.photo.attach(io: file, filename: 'den.jpg', content_type: 'image/png')
 puts Location.all
 
 # Creating a rental for the last location
-puts "creating a rental"
-Rental.create(location: Location.last, user: Location.last.user)
-puts Rental.all
+# puts "creating a rental"
+# Rental.create(location: Location.last, user: Location.last.user)
+# puts Rental.all
