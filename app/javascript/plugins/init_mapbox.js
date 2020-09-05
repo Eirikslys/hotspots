296a369efba4c659204f9397fb48053adeea0c69
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-09-05 14:14:36 +0200

    added gem'geocoder', added 2 new columns to our model: latitude and longtitude, updated model.rb, created token on mapbox,added this key to our.env, some changes in our index method to display map in locations#route, displayed a map in our index view, added mapbox-gl, added markers to our map

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,23 @@
+// importing 'mapboxgl' object from js packages
+import mapboxgl from 'mapbox-gl';
+// then we grab div with with a id "map" on a page
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    // for each marker I can create a new mapbox marker 
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+  }
+};
+
+export { initMapbox };
