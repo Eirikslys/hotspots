aa58c04c922b44dafb17204b1f0d632653246c77
Agata Wojcik <agataizabelawojcik@gmail.com>
2020-09-08 21:28:00 +0200

    created infowindows with pictures and name of location, creating a searchingfield on our map-in progres

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,8 @@
 // importing 'mapboxgl' object from js packages
 import mapboxgl from 'mapbox-gl';
+// import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder';
+
+// import MapboxGeocoder from 'mapbox-gl-geocoder';
 // then we grab div with with a id "map" on a page
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -13,10 +16,27 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     // for each marker I can create a new mapbox marker 
     markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
         .addTo(map);
     });
+    
+    if (markers.length === 0) {
+      map.setZoom(1);
+    } else if (markers.length === 1) {
+      map.setZoom(14);
+      map.setCenter([markers[0].lng, markers[0].lat]);
+    } else {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat]);
+      });
+      map.fitBounds(bounds, { duration: 1000, padding: 90 })
+    }
+      map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
   }
 };
 
