b4dfb6fc96776effbce489583bbad30c12318a88
eirik lysenstoen <elysenstoen@gmail.com>
2020-09-10 20:01:30 +0200

    fixed the datepicker so it will now behave correctly with turbolinks

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,10 +2,12 @@ import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 
-flatpickr(".datepicker", {
-
+const flatpickerfunc = () => {flatpickr(".datepicker", {
   enableTime: true,
-  // altInput: true,
+  altInput: true,
   dateFormat: "Y-m-d H:i",
   plugins: [new rangePlugin({ input: "#rental_end_date"})]
 });
+}
+
+export { flatpickerfunc };
