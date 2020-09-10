d64d29a722637c6c21bcabb9ddc2903bd0cffaa5
eirik lysenstoen <elysenstoen@gmail.com>
2020-09-10 16:21:40 +0200

    made the rental new page look nice, applied dark theme and range plugin

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,11 @@
 import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-flatpickr(".datepicker", {  enableTime: true,   altInput: true,     dateFormat: "Y-m-d H:i"
+
+flatpickr(".datepicker", {
+
+  enableTime: true,
+  // altInput: true,
+  dateFormat: "Y-m-d H:i",
+  plugins: [new rangePlugin({ input: "#rental_end_date"})]
 });
